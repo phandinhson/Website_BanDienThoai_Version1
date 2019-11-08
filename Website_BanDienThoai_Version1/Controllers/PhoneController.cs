77bc3ed989d358af36d663b8fd7390e39b611ede
phan son <phandinhsonlp116@gmail.com>
2019-11-08 19:29:50 +0700

    Add project files.

---
+++ Website_BanDienThoai_Version1/Controllers/PhoneController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Website_BanDienThoai_Version1.Controllers
+{
+    public class PhoneController : Controller
+    {
+        public IActionResult Phone()
+        {
+            return View();
+        }
+    }
+}
